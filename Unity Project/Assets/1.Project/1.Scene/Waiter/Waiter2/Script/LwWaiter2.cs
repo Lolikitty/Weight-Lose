3c0220f21635454004384c982d3962af5e09757c
DemonicKrace <krace710267@gmail.com>
2015-02-02 11:16:43 +0800

    Revert "Update"
    Revert "Update"

---
+++ Unity Project/Assets/1.Project/1.Scene/Waiter/Waiter2/Script/LwWaiter2.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LwWaiter2 : MonoBehaviour {
+
+	public UISlider uis;
+	public GameObject WaiterBottomObj;
+	public TextMesh NowCC;
+
+	public GameObject buttonOk;
+
+	public static int WaiterValue;
+	public static int UseCupCC = 200;
+
+	// Unity Override Methods ==============================================================================================================================
+
+	void Awake () {
+		UIEventListener.Get(buttonOk).onClick = ButtonOk;
+	}
+
+	void Update () {
+		WaiterBottomObj.SetActive(uis.value < 0.02f ? false : true);
+		WaiterValue = (int)(uis.value * UseCupCC);
+		NowCC.text = UseCupCC - WaiterValue + " c.c.";
+	}
+
+	// Custom Methods ======================================================================================================================================
+
+	void ButtonOk(GameObject button){
+		print ("ok : " + WaiterValue + " c.c.");
+		Application.LoadLevel ("Sports");
+	}
+
+}
